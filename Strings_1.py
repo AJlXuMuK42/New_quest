4696da36bef3734f241ce7b6e6894e1b1dea5e45
AJlXuMuK42 <ajlxumuk65@gmail.com>
2024-12-22 19:04:30 +0700

    Add files via upload

---
+++ Strings_1.py
@@ -0,0 +1,8 @@
+example='Bohemian Rhapsody'
+print(example[0])
+print(example[-1])
+print(example[8:])
+print(example[::-1])
+print(example[1::2])
+
+print(19/1.25)
---
+++ Strings_2.py
@@ -0,0 +1,10 @@
+from copy import replace
+
+#name=input('Enter your name: ')
+#current_year=2024
+#date_of_birth=int(input('Where you born? '))
+#age=current_year-date_of_birth
+#print("Hello,", name)
+#print('In this year your', age, 'age')
+print('Suck my duck'.replace('duck', 'dick'))
+
