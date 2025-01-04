df2e6bb44d046550c0fd58a1f7c3f4f0fc07b80c
AJlXuMuK42 <ajlxumuk65@gmail.com>
2025-01-04 23:41:01 +0700

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,11 @@
+first=123
+second=122
+third=124
+if first==second==third:
+    print('3')
+elif first==second or second==third or first==third:
+    print('2')
+elif first!=second!=third:
+    print('0')
+
+
